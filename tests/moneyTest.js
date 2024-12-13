a88609601e9308492e2dd495bc7dc65ec83a7117
Serhas <serhas@gmail.com>
2024-12-13 19:25:59 +0300

    Remove default tests and add money tests

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -10,17 +10,17 @@ if (formatCurrency(2095) === "20.95") {
   console.log("failed");
 }
 
-console.log("rounds up to the nearest cent");
+console.log("works with 0");
 
-if (formatCurrency(2000.5) === "20.01") {
+if (formatCurrency(0) === "0.00") {
   console.log("passed");
 } else {
   console.log("failed");
 }
 
-console.log("works with 0");
+console.log("rounds up to the nearest cent");
 
-if (formatCurrency(0) === "0.00") {
+if (formatCurrency(2000.5) === "20.01") {
   console.log("passed");
 } else {
   console.log("failed");
